58ba7532d71958a7663e499a3e01593acbff25ac
Oliver Ni <oliver.ni@gmail.com>
2020-07-23 13:37:36 -0700

    Update api

--- api/app.py
+++ api/app.py
@@ -1,8 +1,26 @@
 import random
 
+import numpy as np
 from flask import Flask, request
+from scipy.special import softmax
 
 app = Flask(__name__)
+
+
+possible = [
+    "rumor",
+    "hate",
+    "unreliable",
+    "conspiracy",
+    "clickbait",
+    "satire",
+    "fake",
+    "reliable",
+    "bias",
+    "political",
+    "junksci",
+    "unknown",
+]
 
 
 @app.route("/", methods=["POST"])
@@ -10,24 +28,15 @@ def dbl():
     if "url" not in request.json:
         return {"status": "error", "error": "Missing url"}, 400
 
-    result = random.choice(
-        [
-            "rumor",
-            "hate",
-            "unreliable",
-            "conspiracy",
-            "clickbait",
-            "satire",
-            "fake",
-            "reliable",
-            "bias",
-            "political",
-            "junksci",
-            "unknown",
-        ]
-    )
+    category = random.choice(possible)
 
-    return {"status": "success", "result": result}
+    result = [6.0 if i == category else random.random() for i in possible]
+    result = np.array(result)
+    result = softmax(result)
+
+    labeled = {possible[idx]: v for idx, v in enumerate(result)}
+
+    return {"status": "success", "result": labeled}
 
 
 if __name__ == "__main__":
