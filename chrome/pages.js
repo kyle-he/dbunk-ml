9cd80f924d426ef3ab93a4387b38e3312b768175
Kyle He <panyanhe99@gmail.com>
2020-07-24 00:01:27 -0700

    Finish basic functions

---
+++ chrome/pages.js
@@ -0,0 +1,28 @@
+var pages = {
+    reliable: {
+        icon: "fas fa-check-circle fa-5x",
+        color: "#04b401",
+        description: "Our algorithms indicate that this article has a high chance of being credible."
+    },
+    fake: {
+        icon: "fas fa-times-circle fa-5x",
+        color: "#a21010",
+        description: "Our algorithms indicate that this article may contain falsified or twisted information. Please proceed with caution."
+    },
+    bias: {
+        icon: "fas fa-exclamation-triangle fa-5x",
+        color: "#cdaf0a",
+        description: "Our algorithms indicate that this article may contain falsified or twisted information. "
+    },
+    satire: {
+        icon: "fas fa-exclamation-triangle fa-5x",
+        color: "#cdaf0a",
+        description: "Our algorithms indicate that this article may contain falsified or twisted information. "
+    },
+    political: {
+        icon: "fas fa-exclamation-triangle fa-5x",
+        color: "#cdaf0a",
+        description: "Our algorithms indicate that this article may contain falsified or twisted information. "
+    }
+};
+export {pages};
--- chrome/popup.js
+++ chrome/popup.js
@@ -1,32 +1,35 @@
+import {pages} from './pages.js'
+
 chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
-   function(tabs){
+   async function(tabs){
         console.log(tabs[0].url);
-        getScore(tabs[0].url)
+        console.log(getScore(tabs[0].url))
+        const distributions = await getScore(tabs[0].url)
+        getPopup(distributions)
    }
 );
 
 async function getScore(url){
-    console.log(await postData("https://api.dbunk.ml", {url: url}))
+    return (await postData("https://api.dbunk.ml", {url: url}));
 }
 
 function getPopup(score){
+    var highestAttribute = ["unknown", 0]
+    for (const [key,value] of Object.entries(score.result)){
+        if (value > highestAttribute[1]){
+            highestAttribute = [key,value]
+        }
+    }
+    console.log(score)
+    console.log(highestAttribute)
     const oldImg = document.getElementById("status-image")
     const oldText = document.getElementById("status-text")
+
     const newText = document.createElement('div');
     const newImg = document.createElement('div');
-    if (score > 90){
-        // good stuff
-        newImg.innerHTML = '<i  id="status-image" class="fas fa-check-circle fa-5x" style="color:#04b401" ></i>';
-        newText.innerHTML = '<div id="status-text" style="font-size:18px;color:#099400" > Our algorithms indicate that this article has a high chance of being credible. </div>';
-    } else if (score<90 && score>70) {
-        // warning
-        newImg.innerHTML = '<i  id="status-image" class="fas fa-exclamation-triangle fa-5x" style="color:#cdaf0a" ></i>';
-        newText.innerHTML = '<div id="status-text" style="font-size:18px;color:#cdaf0a" >Our algorithms indicate that this article may contain falsified or twisted information. </div>';
-    } else {
-        //super sus article
-        newImg.innerHTML = '<div id="status-image" class="fas fa-times-circle fa-5x" style="color:#a21010" ></div>';
-        newText.innerHTML = '<div id="status-text" style="font-size:18px;color:#a21010" >Our algorithms indicate that this article may contain falsified or twisted information. Please proceed with caution.</div>';
-    }
+    newImg.innerHTML = `<i  id="status-image" class="${pages[highestAttribute[0]].icon}" style="color:${pages[highestAttribute[0]].color}" ></i>`;
+    newText.innerHTML = `<div id="status-text" style="font-size:18px;color:${pages[highestAttribute[0]].color}" > ${pages[highestAttribute[0]].description} </div>`;
+    
     oldImg.parentNode.replaceChild(newImg, oldImg);
     oldText.parentNode.replaceChild(newText, oldText);
 }
