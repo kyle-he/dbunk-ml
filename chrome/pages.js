a604b84bc267643432e2500ee47a4f6990f73be5
Oliver Ni <oliver.ni@gmail.com>
2020-07-25 13:07:40 -0700

    Polish extension

--- chrome/pages.js
+++ chrome/pages.js
@@ -2,32 +2,38 @@ var pages = {
     reliable: {
         icon: "fas fa-check-circle fa-5x",
         color: "#04b401",
-        description: "Our algorithms indicate that this article has a high chance of being credible."
+        description:
+            "Our algorithms indicate that this article has a high chance of being credible.",
     },
     fake: {
         icon: "fas fa-times-circle fa-5x",
         color: "#a21010",
-        description: "Our algorithms indicate that this article may contain falsified or twisted information. Please proceed with caution."
+        description:
+            "Our algorithms indicate that this article may contain falsified or twisted information. Proceed with caution.",
     },
     bias: {
         icon: "fas fa-exclamation-triangle fa-5x",
         color: "#cdaf0a",
-        description: "Our algorithms indicate that this article may contain falsified or twisted information. "
+        description:
+            "Our algorithms indicate that this article may be heavily biased. Proceed with caution.",
     },
     satire: {
         icon: "fas fa-exclamation-triangle fa-5x",
         color: "#cdaf0a",
-        description: "Our algorithms indicate that this article may contain falsified or twisted information. "
+        description:
+            "Our algorithms indicate that this article contains satire.",
     },
     political: {
         icon: "fas fa-exclamation-triangle fa-5x",
         color: "#cdaf0a",
-        description: "Our algorithms indicate that this article may contain falsified or twisted information. "
+        description:
+            "Our algorithms indicate that this article is heavily political.",
     },
     unknown: {
         icon: "fas fa-exclamation-triangle fa-5x",
         color: "#cdaf0a",
-        description: "Our algorithms indicate that this article may contain falsified or twisted information. "
-    }
+        description:
+            "Our algorithms weren't able to identify the nature of this article.",
+    },
 };
-export {pages};
+export { pages };
--- chrome/popup.js
+++ chrome/popup.js
@@ -1,105 +1,155 @@
-import {pages} from './pages.js'
-import {bias} from './bias.js'
+import { pages } from "./pages.js";
+import { bias } from "./bias.js";
 
-chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
-   async function(tabs){
-        console.log(getScore(tabs[0].url))
-        const distributions = await getScore(tabs[0].url)
-        var simplifiedURL = tabs[0].url.toLowerCase().replace("\\", "").replace("http://", "https://").replace("www.", "");
-        console.log(simplifiedURL)
-        getPopup(distributions, simplifiedURL)
-   }
+chrome.tabs.query(
+    { active: true, windowId: chrome.windows.WINDOW_ID_CURRENT },
+    async function (tabs) {
+        console.log(getScore(tabs[0].url));
+        const distributions = await getScore(tabs[0].url);
+        var simplifiedURL = tabs[0].url
+            .toLowerCase()
+            .replace("\\", "")
+            .replace("http://", "https://")
+            .replace("www.", "");
+        console.log(simplifiedURL);
+        getPopup(distributions, simplifiedURL);
+    }
 );
 
-function getScore(url){
-    return postData("https://api.dbunk.ml", {url: url});
+function getScore(url) {
+    return postData("https://api.dbunk.ml", { url: url });
 }
 
-function getSite(url){
-    for (var site of bias){
+function getSite(url) {
+    for (var site of bias) {
         try {
-            if (url.includes(site.url.toLowerCase().replace("\\", "").replace("http://", "https://").replace("www.", "")) && site.url != ""){
-                return{title: site.news_source, 
-                       allsides_url: site.allsides_url, 
-                       bias_rating: site.bias_rating,
-                       retrieved: true
-                    }
+            if (
+                url.includes(
+                    site.url
+                        .toLowerCase()
+                        .replace("\\", "")
+                        .replace("http://", "https://")
+                        .replace("www.", "")
+                ) &&
+                site.url != ""
+            ) {
+                return {
+                    title: site.news_source,
+                    allsides_url: site.allsides_url,
+                    bias_rating: site.bias_rating,
+                    retrieved: true,
+                };
             }
-        } catch(TypeError) {}
+        } catch (TypeError) {}
     }
-    return{
-        retrieved: false
-    }
+    return {
+        retrieved: false,
+    };
 }
-function getDescrip(url){
+function getDescrip(url) {
     var name = "temp";
-    $.get(url, function(response) {  
-        var challenges = $(response).find('latest_news_source');
-        console.log(challenges)
+    $.get(url, function (response) {
+        var challenges = $(response).find("latest_news_source");
+        console.log(challenges);
     });
 }
-function getPopup(score, url){
-    var siteStats = getSite(url)
+function getPopup(score, url) {
+    var siteStats = getSite(url);
     var ratingObjs = {
-        "71": {"img": "images/bias-left.png", "alt": "Left bias",
-            "desc": "This site tends to be biased to the left. This trend reflects the site as a whole and not any specific article."},
-        "72": {"img": "images/bias-leaning-left.png", "alt": "Leaning left bias",
-            "desc": "This site tends to be slightly biased to the left. This trend reflects the site as a whole and not any specific article."},
-        "73": {"img": "images/bias-center.png", "alt": "Center bias",
-            "desc": "This site tends to be centrally aligned. This trend reflects the site as a whole and not any specific article."},
-        "74": {"img": "images/bias-leaning-right.png", "alt": "Leaning right bias",
-            "desc": "This site tends to be slightly biased to the right. This trend reflects the site as a whole and not any specific article."},
-        "75": {"img": "images/bias-right.png", "alt": "Right bias",
-            "desc": "This site tends to be biased to the right. This trend reflects the site as a whole and not any specific article."},
-        "2707": {"img": "images/bias-mixed.png", "alt": "Mixed bias",
-            "desc": "This site has a very mixed alignment, or simply doesn't fall on the left/right partisanship scale."},
-        "2690": {"img": "images/bias-not-yet-rated.png", "alt": "Site not rated",
-            "desc": "This site has not yet been rated."}
+        "71": {
+            img: "images/bias-left.png",
+            alt: "Left bias",
+            desc:
+                "This site tends to be biased to the left. This trend reflects the site as a whole and not any specific article.",
+        },
+        "72": {
+            img: "images/bias-leaning-left.png",
+            alt: "Leaning left bias",
+            desc:
+                "This site tends to be slightly biased to the left. This trend reflects the site as a whole and not any specific article.",
+        },
+        "73": {
+            img: "images/bias-center.png",
+            alt: "Center bias",
+            desc:
+                "This site tends to be centrally aligned. This trend reflects the site as a whole and not any specific article.",
+        },
+        "74": {
+            img: "images/bias-leaning-right.png",
+            alt: "Leaning right bias",
+            desc:
+                "This site tends to be slightly biased to the right. This trend reflects the site as a whole and not any specific article.",
+        },
+        "75": {
+            img: "images/bias-right.png",
+            alt: "Right bias",
+            desc:
+                "This site tends to be biased to the right. This trend reflects the site as a whole and not any specific article.",
+        },
+        "2707": {
+            img: "images/bias-mixed.png",
+            alt: "Mixed bias",
+            desc:
+                "This site has a very mixed alignment, or simply doesn't fall on the left/right partisanship scale.",
+        },
+        "2690": {
+            img: "images/bias-not-yet-rated.png",
+            alt: "Site not rated",
+            desc: "This site has not yet been rated.",
+        },
     };
-    if  (siteStats.retrieved){
-        const oldTitle = document.getElementById("title")
-        const oldRating = document.getElementById("bias")
-        const oldDescrip = document.getElementById("bias-descrip")
-    
-        const newTitle = document.createElement('div');
-        const newRating = document.createElement('span');
-        const newDescrip = document.createElement('div');
 
-        newTitle.innerHTML = `<div id="title" class="title normal_text no_padding">${siteStats.title}</div>`;
-        newRating.innerHTML = `<span id="bias">${ratingObjs[siteStats.bias_rating].alt}</span>`;
-        newDescrip.innerHTML = `<div id="bias-descrip">${ratingObjs[siteStats.bias_rating].desc}</div>`;
+    if (siteStats.retrieved) {
+        const title = document.getElementById("site-title");
+        const rating = document.getElementById("site-rating");
+        const descrip = document.getElementById("bias-descrip");
 
-        oldTitle.parentNode.replaceChild(newTitle, oldTitle);
-        oldRating.parentNode.replaceChild(newRating, oldRating);
-        oldDescrip.parentNode.replaceChild(newDescrip, oldDescrip);
+        const newTitle = document.createElement("div");
+        const newRating = document.createElement("span");
+        const newDescrip = document.createElement("div");
+
+        title.innerHTML = siteStats.title;
+        rating.innerHTML = ratingObjs[siteStats.bias_rating].alt;
+        descrip.innerHTML = ratingObjs[siteStats.bias_rating].desc;
     }
-    var highestAttribute = ["unknown", 0]
-    for (const [key,value] of Object.entries(score.result)){
-        if (value > highestAttribute[1]){
-            highestAttribute = [key,value]
+
+    var highestAttribute = ["unknown", 0];
+    for (const [key, value] of Object.entries(score.result)) {
+        if (value > highestAttribute[1]) {
+            highestAttribute = [key, value];
         }
     }
-    console.log(score)
-    console.log(highestAttribute)
-    const oldImg = document.getElementById("status-image")
-    const oldText = document.getElementById("status-text")
 
-    const newText = document.createElement('div');
-    const newImg = document.createElement('div');
-    newImg.innerHTML = `<i  id="status-image" class="${pages[highestAttribute[0]].icon}" style="color:${pages[highestAttribute[0]].color}" ></i>`;
-    newText.innerHTML = `<div id="status-text" style="font-size:18px;color:${pages[highestAttribute[0]].color}" > ${highestAttribute[0].charAt(0).toUpperCase() + highestAttribute[0].slice(1)} with ${highestAttribute[1].toFixed(3)*100}% confidence. ${pages[highestAttribute[0]].description} </div>`;
-    
-    oldImg.parentNode.replaceChild(newImg, oldImg);
-    oldText.parentNode.replaceChild(newText, oldText);
+    console.log(score);
+    console.log(highestAttribute);
+
+    const img = document.getElementById("status-image");
+    const text = document.getElementById("status-text");
+    const confidence = document.getElementById("status-confidence");
+    const subtext = document.getElementById("status-subtext");
+
+    img.innerHTML = `<i class="${
+        pages[highestAttribute[0]].icon
+    }" style="color:${pages[highestAttribute[0]].color}"></i>`;
+
+    text.style.color = pages[highestAttribute[0]].color;
+
+    text.innerHTML =
+        highestAttribute[0].charAt(0).toUpperCase() +
+        highestAttribute[0].slice(1);
+    confidence.innerHTML = `Confidence: ${
+        highestAttribute[1].toFixed(3) * 100
+    }%`;
+    subtext.innerHTML = pages[highestAttribute[0]].description;
 }
 
 async function postData(url, data = {}) {
     const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(data)
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
     });
     return response.json();
 }
